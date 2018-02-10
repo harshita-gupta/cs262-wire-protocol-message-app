e566a22a6f67a19c8113413f9c6be4ebfce4e906
harshita-gupta <gupta.harshita.001@gmail.com>
2018-02-10 18:39:04 -0500

    first draft of wire protocol

--- MessageAppProtocol.java
+++ MessageAppProtocol.java
@@ -50,12 +50,12 @@ package edu.harvard.cs262.atm;
  * encoding for strings. Behavior not specified in this document is left as an
  * implementation decision for the particular client and server.
  */
-public class ATMProtocol {
+public class MessageAppProtocol {
 
     /**
      * This class should not be instantiated.
      */
-    protected ATMProtocol() {
+    protected MessageAppProtocol() {
         throw new UnsupportedOperationException();
     }
 
@@ -82,16 +82,13 @@ public class ATMProtocol {
      * Opcode: {@code 0x10} <br>
      * Payload:
      * <ul>
-     * <li>(int) starting_balance
-     * <li>(int) account_number
+     * <li>(String) username
      * </ul>
      *
      * <p>
-     * The {@code account_number} is an optional field. If the user supplies a
-     * desired account number, the client should send it in the {@code
-     * account_number} field. The {@code account_number} field should be between
-     * 1 and 100 if valid. If the user does not supply a desired account number,
-     * the value should be {@code -1}. After the server processes the creation
+     * The {@code username} must only contain alphanumeric characters.
+     * Usernames are case-sensitive.
+     * After the server processes the creation
      * request, the server sends one of the following acknowledgments to the
      * client:
      * <ul>
@@ -107,10 +104,9 @@ public class ATMProtocol {
      * Opcode: {@code 0x11} <br>
      * Payload:
      * <ul>
-     * <li>(int) account_number
+     * <li>(int) username
      * </ul>
-     * The {@code account_number} is the number of the account the server just
-     * created.
+     * The {@code account_number} is the username the server just created.
      */
     public static final byte CREATE_SUCCESS = 0x11;
 
@@ -125,9 +121,8 @@ public class ATMProtocol {
      * This message indicates that the server rejects the
      * {@link #CREATE_REQUEST} for some reason, for example:
      * <ul>
-     * <li>invalid desired account number,
-     * <li>unavailable desired account number,
-     * <li>invalid starting balance.
+     * <li> invalid desired username (not alphanumeric)
+     * <li> unavailable desired username
      * </ul>
      *
      */
@@ -140,7 +135,7 @@ public class ATMProtocol {
      * Opcode: {@code 0x20} <br>
      * Payload:
      * <ul>
-     * <li>(int) account_number
+     * <li>(String) username
      * </ul>
      *
      * <p>
@@ -173,176 +168,220 @@ public class ATMProtocol {
      * This message indicates that the server rejects the
      * {@link #DELETE_REQUEST} for some reason, for example:
      * <ul>
-     * <li>invalid account number,
-     * <li>nonzero account balance.
+     * <li> invalid username (account might've already been deleted)
+     * <li> message delivery to that user is pending
      * </ul>
      */
     public static final byte DELETE_FAILURE = 0x22;
 
     /**
-     * Account deposit request from a client to a server. This message should
+     * Send message request from a client to a server. This message should
      * only be sent to a server; if received by a client it should be ignored. <br>
      * Opcode: {@code 0x30} <br>
      * Payload:
      * <ul>
-     * <li>(int) account_number
-     * <li>(int) amount
+     * <li>(String) sending_user
+     * <li>(String) receiving_user
+     * <li>(String) message
      * </ul>
      *
      * <p>
-     * The {@code account_number} is the number of the account that should
-     * receive the deposit. The {@code amount} should be a positive whole-number
-     * amount of USD to be deposited in the specified account. After the server
-     * processes the deposit request, the server sends one of the following
+     * The {@code receiving_user} is the username of the user that should
+     * receive the message. The {@code sending_user} is the user that is sending the message.
+     * The {@code message} should be the content of the message.
+     * After the server processes the message request, the server sends one of the following
      * acknowledgments to the client:
      * <ul>
-     * <li> {@link #DEPOSIT_SUCCESS}, if account deposit succeeds.
-     * <li>{@link #DEPOSIT_FAILURE}, if account deposit fails.
+     * <li> {@link #SEND_MESSAGE_SUCCESS}, if message sending succeeds.
+     * <li>{@link #SEND_MESSAGE_FAILURE}, if message sending fails.
      * </ul>
      */
-    public static final byte DEPOSIT_REQUEST = 0x30;
+    public static final byte SEND_MESSAGE_REQUEST = 0x30;
 
     /**
-     * Deposit success. Server response to a client's {@link #DEPOSIT_REQUEST}. <br>
+     * Send message success. Server response to a client's {@link #SEND_MESSAGE_REQUEST}. <br>
      * Opcode: {@code 0x31} <br>
      * Payload:
      * <ul>
-     * <li>(int) balance
+     * <li>(String) receiving_user
      * </ul>
      *
-     * The {@code balance} returned reflects the balance in the specified
-     * account after {@code amount} is deposited.
+     * The {@code receiving_user} returned reflects the user who received the request.
      */
-    public static final byte DEPOSIT_SUCCESS = 0x31;
+    public static final byte SEND_MESSAGE_SUCCESS = 0x31;
 
     /**
-     * Deposit failure. Server response to a client's {@link #DEPOSIT_REQUEST}. <br>
+     * Send message failure. Server response to a client's {@link #SEND_MESSAGE_REQUEST}. <br>
      * Opcode: {@code 0x32} <br>
      * Payload:
      * <ul>
      * <li>(String) reason
      * </ul>
      * This message indicates that the server rejects the
-     * {@link #DEPOSIT_REQUEST} for some reason, for example:
+     * {@link #SEND_MESSAGE_REQUEST} for some reason, for example:
      * <ul>
-     * <li>invalid account number,
-     * <li>invalid deposit amount.
+     * <li>invalid sending user (might have been deleted from another client)
+     * <li>invalid receiving user (might have been deleted after users were last listed, by another client)
      * </ul>
      *
-     * Note that we assume a client has less than 2^32-1 dollars in a single
-     * account. This would likely not be the case even if a client had 2^32-1
-     * dollars due to FDIC limitations as well as practical money-management
-     * issues (one would likely play the market with a portion, etc.).
      */
-    public static final byte DEPOSIT_FAILURE = 0x32;
+    public static final byte SEND_MESSAGE_FAILURE = 0x32;
 
     /**
-     * Withdrawal request from a client to a server. This message should only be
-     * sent to a server; if received by a client it should be ignored. <br>
-     * Opcode: {@code 0x40} <br>
+     * Deliver message request from a server to a client. This message should
+     * only be sent to a client; if received by a server it should be ignored. <br>
+     * Opcode: {@code 0x80} <br>
      * Payload:
      * <ul>
-     * <li>(int) account_number
-     * <li>(int) amount
+     * <li>(String) sending_user
+     * <li>(String) receiving_user
+     * <li>(String) message
      * </ul>
      *
      * <p>
-     * The {@code account_number} is the number of the account from which the
-     * money should be withdrawn. The {@code amount} should be a positive
-     * whole-number amount of USD to be withdrawn the specified account not to
-     * exceed the account's current balance. After the server processes the
-     * withdrawal request, the server sends one of the following acknowledgments
-     * to the client:
+     * The {@code receiving_user} is the username of the user that should
+     * receive the message. The {@code sending_user} is the user that is sending the message.
+     * The {@code message} should be the content of the message.
+     * After the server processes the message request, the server sends one of the following
+     * acknowledgments to the client:
      * <ul>
-     * <li> {@link #WITHDRAW_SUCCESS}, if account withdrawal succeeds.
-     * <li>{@link #WITHDRAW_FAILURE}, if account withdrawal fails.
+     * <li> {@link #DELIVER_MESSAGE_SUCCESS}, if message sending succeeds.
+     * <li>{@link #DELIVER_MESSAGE_FAILURE}, if message sending fails.
      * </ul>
      */
-    public static final byte WITHDRAW_REQUEST = 0x40;
+    public static final byte DELIVER_MESSAGE_REQUEST = 0x80;
 
     /**
-     * Withdrawal success. Server response to a client's {@link #WITHDRAW_REQUEST}. <br>
-     * Opcode: {@code 0x41} <br>
+     * Send message success. Client response to a server's {@link #DELIVER_MESSAGE_REQUEST}. <br>
+     * Opcode: {@code 0x81} <br>
      * Payload:
      * <ul>
-     * <li>(int) balance
+     * <li>(String) receiving_user
      * </ul>
      *
-     * The {@code balance} returned reflects the balance in the specified
-     * account after {@code amount} is deposited.
+     * The {@code receiving_user} returned reflects the user who received the request.
      */
-    public static final byte WITHDRAW_SUCCESS = 0x41;
+    public static final byte DELIVER_MESSAGE_SUCCESS = 0x81;
 
     /**
-     * Withdrawal failure. Server response to a client's {@link #WITHDRAW_REQUEST}. <br>
-     * Opcode: {@code 0x42} <br>
+     * Send message failure. Client response to a server's {@link #DELIVER_MESSAGE_REQUEST}. <br>
+     * Opcode: {@code 0x82} <br>
      * Payload:
      * <ul>
      * <li>(String) reason
      * </ul>
-     * This message indicates that the server rejects the
-     * {@link #WITHDRAW_REQUEST} for some reason, for example:
+     * This message indicates that the client rejects the
+     * {@link #DELIVER_MESSAGE_REQUEST} for some reason, for example:
      * <ul>
-     * <li>invalid account number,
-     * <li>insufficient funds,
-     * <li>invalid deposit amount.
-     * </ul>
-     */
-    public static final byte WITHDRAW_FAILURE = 0x42;
-
-    /**
-     * Balance request from a client to a server. Requests the remaining balance
-     * in a given account. This message should only be sent to a server; if
-     * received by a client it should be ignored. <br>
-     * Opcode: {@code 0x50} <br>
-     * Payload:
-     * <ul>
-     * <li>(int) account_number
+     * <li> User that the message was intended for is not the one currently logged in.
      * </ul>
      *
+     */
+    public static final byte DELIVER_MESSAGE_FAILURE = 0x82;
+
+    // /**
+    //  * Withdrawal request from a client to a server. This message should only be
+    //  * sent to a server; if received by a client it should be ignored. <br>
+    //  * Opcode: {@code 0x40} <br>
+    //  * Payload:
+    //  * <ul>
+    //  * <li>(int) account_number
+    //  * <li>(int) amount
+    //  * </ul>
+    //  *
+    //  * <p>
+    //  * The {@code account_number} is the number of the account from which the
+    //  * money should be withdrawn. The {@code amount} should be a positive
+    //  * whole-number amount of USD to be withdrawn the specified account not to
+    //  * exceed the account's current balance. After the server processes the
+    //  * withdrawal request, the server sends one of the following acknowledgments
+    //  * to the client:
+    //  * <ul>
+    //  * <li> {@link #WITHDRAW_SUCCESS}, if account withdrawal succeeds.
+    //  * <li>{@link #WITHDRAW_FAILURE}, if account withdrawal fails.
+    //  * </ul>
+    //  */
+    // public static final byte WITHDRAW_REQUEST = 0x40;
+
+    // /**
+    //  * Withdrawal success. Server response to a client's {@link #WITHDRAW_REQUEST}. <br>
+    //  * Opcode: {@code 0x41} <br>
+    //  * Payload:
+    //  * <ul>
+    //  * <li>(int) balance
+    //  * </ul>
+    //  *
+    //  * The {@code balance} returned reflects the balance in the specified
+    //  * account after {@code amount} is deposited.
+    //  */
+    // public static final byte WITHDRAW_SUCCESS = 0x41;
+
+    // /**
+    //  * Withdrawal failure. Server response to a client's {@link #WITHDRAW_REQUEST}. <br>
+    //  * Opcode: {@code 0x42} <br>
+    //  * Payload:
+    //  * <ul>
+    //  * <li>(String) reason
+    //  * </ul>
+    //  * This message indicates that the server rejects the
+    //  * {@link #WITHDRAW_REQUEST} for some reason, for example:
+    //  * <ul>
+    //  * <li>invalid username,
+    //  * <li>insufficient funds,
+    //  * <li>invalid deposit amount.
+    //  * </ul>
+    //  */
+    // public static final byte WITHDRAW_FAILURE = 0x42;
+
+    /**
+     * List users request from a client to a server.
+     * This message should only be sent to a server; if
+     * received by a client it should be ignored. <br>
+     * Opcode: {@code 0x50} <br>
+     * No payload.
+     *
      * <p>
-     * The {@code account_number} is the number of the account whose balance
-     * should be returned. After the server processes the account balance check
+     * After the server processes the account balance check
      * request, the server sends one of the following acknowledgments to the
      * client:
      * <ul>
-     * <li> {@link #BALANCE_SUCCESS}, if account balance check succeeds.
-     * <li>{@link #BALANCE_FAILURE}, if account balance check fails.
+     * <li> {@link #LIST_USERS_SUCCESS}, if users list requests succeeds.
+     * <li>{@link #LIST_USERS_FAILURE}, if users list requests fails.
      * </ul>
      */
-    public static final byte BALANCE_REQUEST = 0x50;
+    public static final byte LIST_USERS_REQUEST = 0x50;
 
     /**
-     * Balance check success. Server response to a client's
-     * {@link #BALANCE_REQUEST}. <br>
+     * List users success. Server response to a client's
+     * {@link #LIST_USERS_REQUEST}. <br>
      * Opcode: {@code 0x51} <br>
      * Payload:
      * <ul>
-     * <li>(int) balance
+     * <li> (String) all_users
      * </ul>
-     *
-     * The {@code balance} returned reflects the current balance in the
-     * specified account.
+     *  <p>
+     * {@code all_users} is a new-line separated list of all users currently
+     * registered (who have not been deleted).
      */
-    public static final byte BALANCE_SUCCESS = 0x51;
+    public static final byte LIST_USERS_SUCCESS = 0x51;
 
     /**
-     * Deposit failure. Server response to a client's {@link #BALANCE_REQUEST}. <br>
+     * List users failure. Server response to a client's {@link #LIST_USERS_REQUEST}. <br>
      * Opcode: {@code 0x52} <br>
      * Payload:
      * <ul>
      * <li>(String) reason
      * </ul>
      * This message indicates that the server rejects the
-     * {@link #BALANCE_REQUEST} for some reason, for example:
+     * {@link #LIST_USERS_REQUEST} for some reason, for example:
      * <ul>
-     * <li>invalid account number.
+     * <li> no users exist.
      * </ul>
      */
-    public static final byte BALANCE_FAILURE = 0x52;
+    public static final byte LIST_USERS_FAILURE = 0x52;
 
     /**
-     * Request from a client to a server to disconnect and shut down the ATM.
+     * Request from a client to a server to disconnect that the user is logging out.
      * This message should only be sent to a server; if received by a client it
      * should be ignored. <br>
      * Opcode: {@code 0x60} <br>
@@ -353,23 +392,65 @@ public class ATMProtocol {
      * intent to shut down. (There might be cases where the bank wants to keep
      * the ATM up and running, though we omit them in this protocol). After the
      * server processes the disconnect request, the server sends
-     * {@link #END_SESSION_SUCCESS} to the client.
+     * {@link #LOG_OUT_SUCCESS} to the client.
      */
-    public static final byte END_SESSION = 0x60;
+    public static final byte LOG_OUT = 0x60;
 
     /**
-     * End session success. Server response to a client's {@link #END_SESSION}. <br>
+     * End session success. Server response to a client's {@link #LOG_OUT}. <br>
      * Opcode: {@code 0x61} <br>
      * Payload: None.
      */
-    public static final byte END_SESSION_SUCCESS = 0x61;
+    public static final byte LOG_OUT_SUCCESS = 0x61;
 
     /**
      * Message from the server to the client indicating that a message with an
      * unknown opcode has been received. On the second receipt of an unknown
      * opcode, the server will disconnect the client, after sending an
-     * {@link #END_SESSION_SUCCESS} message. Opcode: {@code Ox62}<br>
+     * {@link #LOG_OUT_SUCCESS} message. Opcode: {@code Ox62}<br>
      * Payload: None.
      */
     public static final byte UNKNOWN_OPCODE = 0x62;
+
+     /**
+     * Notification from a client to a server that a user wishes to log in.
+     * This message should only be sent to a server; if received by a client it
+     * should be ignored. <br>
+     * Opcode: {@code 0x70} <br>
+     * Payload:
+     * <ul>
+     * <li> (String) username
+     * <p>
+     * After the server processes the log in request
+     * the server sends one of the following acknowledgments to the
+     * client:
+     * <ul>
+     * <li> {@link #LOG_IN_SUCCESS}, if log in succeeds.
+     * <li> {@link #LOG_IN_FAILURE}, if log in fails.
+     * </ul>
+     * <p>
+     */
+    public static final byte LOG_IN = 0x70;
+
+    /**
+     * Log in success. Server response to a client's {@link #LOG_IN}. <br>
+     * Opcode: {@code 0x71} <br>
+     * Payload: None.
+     */
+    public static final byte LOG_IN_SUCCESS = 0x71;
+
+    /**
+     * Log in failure. Server response to a client's {@link #LOG_IN}. <br>
+     * Opcode: {@code 0x72} <br>
+     * Payload:
+     * <ul>
+     * <li>(String) reason
+     * </ul>
+     * This message indicates that the server rejects the
+     * {@link #LOG_IN_REQUEST} for some reason, for example:
+     * <ul>
+     * <li> no user with the username specified exists.
+     * </ul>
+     */
+    public static final byte LOG_IN_FAILURE = 0x72;
 }
