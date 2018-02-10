ca8fefd5ec8248ef2eb6269c59b3b5d6a2266f2e
harshita-gupta <gupta.harshita.001@gmail.com>
2018-02-10 17:28:27 -0500

    added wire protocol template

---
+++ MessageAppProtocol.java
@@ -0,0 +1,375 @@
+package edu.harvard.cs262.atm;
+
+/**
+ * ATM Protocol constants.
+ * <p>
+ * A protocol message is constructed as follows:
+ * <ul>
+ * <li>A header, consisting of
+ * <ul>
+ * <li>(byte) protocol version number
+ * <li>(int) payload length, not including this field, the version field, or the
+ * opcode
+ * <li>(byte) operation code
+ * </ul>
+ * <li>optionally, additional payload, specified for each opcode
+ * </ul>
+ * <p>
+ * If the additional payload includes a {@code String}, it will be encoded as
+ * follows:
+ * <ul>
+ * <li>(short) number of bytes
+ * <li>(byte[]) String encoded in the UTF-8 character set
+ *
+ * </ul>
+ * <p>
+ * Because there is no "start session" command in the protocol that will
+ * establish a connection to the server, we always send the protocol version
+ * number. Note that since there is no login operation, the server is expected
+ * to accept any socket connection from any client.
+ * <p>
+ * The {@code protocol version} will be checked by the server to ensure that the
+ * client and server are using the same protocol or versions of the protocol
+ * that are compatible. If the server determines that the protocol version used
+ * by the client and the protocol version or versions required by the server are
+ * not compatible, the server will disconnect the client. In cases where the
+ * protocols being used are not compatible, no other communication between the
+ * client and the server is guaranteed to be understood.
+ * <p>
+ * The total length of a message must not be greater than 1024 bytes; given the
+ * header information this means that the payload of a message cannot be greater
+ * than 1018 bytes. This only limits the size of the error strings that are sent
+ * back to the client, but these should be kept reasonably short anyway. This
+ * also means that the buffers that are allocated for the sending and receiving
+ * of messages can be kept to a more reasonable size.
+ * <p>
+ * We follow the Java language definitions of the basic data types. In
+ * particular, a {@code byte} is defined as 8 bits, and an {@code int} is defined
+ * as 4 bytes (32 bits). {@code String} representations will vary from
+ * environment to environment, which is why we define UTF-8 as the transfer
+ * encoding for strings. Behavior not specified in this document is left as an
+ * implementation decision for the particular client and server.
+ */
+public class ATMProtocol {
+
+    /**
+     * This class should not be instantiated.
+     */
+    protected ATMProtocol() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * The maximum length of a protocol message: {@value #MAX_MESSAGE_LENGTH}
+     * bytes.
+     */
+    public static final int MAX_MESSAGE_LENGTH = 1024;
+
+    public static final int HEADER_SIZE = 6;
+
+    /**
+     * The maximum payload length: {@value #MAX_PAYLOAD_LENGTH} bytes.
+     */
+    public static final int MAX_PAYLOAD_LENGTH = MAX_MESSAGE_LENGTH
+            - HEADER_SIZE;
+
+    /** The version number, currently {@code 0x01}. */
+    public static final byte VERSION = 0x01;
+
+    /**
+     * Account creation request from a client to a server. This message should
+     * only be sent to a server; if received by a client it should be ignored. <br>
+     * Opcode: {@code 0x10} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) starting_balance
+     * <li>(int) account_number
+     * </ul>
+     *
+     * <p>
+     * The {@code account_number} is an optional field. If the user supplies a
+     * desired account number, the client should send it in the {@code
+     * account_number} field. The {@code account_number} field should be between
+     * 1 and 100 if valid. If the user does not supply a desired account number,
+     * the value should be {@code -1}. After the server processes the creation
+     * request, the server sends one of the following acknowledgments to the
+     * client:
+     * <ul>
+     * <li> {@link #CREATE_SUCCESS}, if account creation succeeds.
+     * <li> {@link #CREATE_FAILURE}, if account creation fails.
+     * </ul>
+     */
+    public static final byte CREATE_REQUEST = 0x10;
+
+    /**
+     * Account creation success. Server response to a client's
+     * {@link #CREATE_REQUEST}. <br>
+     * Opcode: {@code 0x11} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) account_number
+     * </ul>
+     * The {@code account_number} is the number of the account the server just
+     * created.
+     */
+    public static final byte CREATE_SUCCESS = 0x11;
+
+    /**
+     * Account creation failure. Server response to a client's
+     * {@link #CREATE_REQUEST}. <br>
+     * Opcode: {@code 0x12} <br>
+     * Payload:
+     * <ul>
+     * <li>(String) reason
+     * </ul>
+     * This message indicates that the server rejects the
+     * {@link #CREATE_REQUEST} for some reason, for example:
+     * <ul>
+     * <li>invalid desired account number,
+     * <li>unavailable desired account number,
+     * <li>invalid starting balance.
+     * </ul>
+     *
+     */
+    public static final byte CREATE_FAILURE = 0x12;
+
+    /**
+     * Account destruction request from a client to a server. This message
+     * should only be sent to a server; if received by a client it should be
+     * ignored. <br>
+     * Opcode: {@code 0x20} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) account_number
+     * </ul>
+     *
+     * <p>
+     * The {@code account_number} is the number of the account to be deleted.
+     * After the server processes the deletion request, the server sends one of
+     * the following acknowledgments to the client:
+     * <ul>
+     * <li> {@link #DELETE_SUCCESS}, if account deletion succeeds.
+     * <li>{@link #DELETE_FAILURE}, if account deletion fails.
+     * </ul>
+     */
+    public static final byte DELETE_REQUEST = 0x20;
+
+    /**
+     * Account deletion success. Server response to a client's
+     * {@link #DELETE_REQUEST}. <br>
+     * Opcode: {@code 0x21} <br>
+     * Payload: none
+     */
+    public static final byte DELETE_SUCCESS = 0x21;
+
+    /**
+     * Account deletion failure. Server response to a client's
+     * {@link #DELETE_REQUEST}. <br>
+     * Opcode: {@code 0x22} <br>
+     * Payload:
+     * <ul>
+     * <li>(String) reason
+     * </ul>
+     * This message indicates that the server rejects the
+     * {@link #DELETE_REQUEST} for some reason, for example:
+     * <ul>
+     * <li>invalid account number,
+     * <li>nonzero account balance.
+     * </ul>
+     */
+    public static final byte DELETE_FAILURE = 0x22;
+
+    /**
+     * Account deposit request from a client to a server. This message should
+     * only be sent to a server; if received by a client it should be ignored. <br>
+     * Opcode: {@code 0x30} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) account_number
+     * <li>(int) amount
+     * </ul>
+     *
+     * <p>
+     * The {@code account_number} is the number of the account that should
+     * receive the deposit. The {@code amount} should be a positive whole-number
+     * amount of USD to be deposited in the specified account. After the server
+     * processes the deposit request, the server sends one of the following
+     * acknowledgments to the client:
+     * <ul>
+     * <li> {@link #DEPOSIT_SUCCESS}, if account deposit succeeds.
+     * <li>{@link #DEPOSIT_FAILURE}, if account deposit fails.
+     * </ul>
+     */
+    public static final byte DEPOSIT_REQUEST = 0x30;
+
+    /**
+     * Deposit success. Server response to a client's {@link #DEPOSIT_REQUEST}. <br>
+     * Opcode: {@code 0x31} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) balance
+     * </ul>
+     *
+     * The {@code balance} returned reflects the balance in the specified
+     * account after {@code amount} is deposited.
+     */
+    public static final byte DEPOSIT_SUCCESS = 0x31;
+
+    /**
+     * Deposit failure. Server response to a client's {@link #DEPOSIT_REQUEST}. <br>
+     * Opcode: {@code 0x32} <br>
+     * Payload:
+     * <ul>
+     * <li>(String) reason
+     * </ul>
+     * This message indicates that the server rejects the
+     * {@link #DEPOSIT_REQUEST} for some reason, for example:
+     * <ul>
+     * <li>invalid account number,
+     * <li>invalid deposit amount.
+     * </ul>
+     *
+     * Note that we assume a client has less than 2^32-1 dollars in a single
+     * account. This would likely not be the case even if a client had 2^32-1
+     * dollars due to FDIC limitations as well as practical money-management
+     * issues (one would likely play the market with a portion, etc.).
+     */
+    public static final byte DEPOSIT_FAILURE = 0x32;
+
+    /**
+     * Withdrawal request from a client to a server. This message should only be
+     * sent to a server; if received by a client it should be ignored. <br>
+     * Opcode: {@code 0x40} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) account_number
+     * <li>(int) amount
+     * </ul>
+     *
+     * <p>
+     * The {@code account_number} is the number of the account from which the
+     * money should be withdrawn. The {@code amount} should be a positive
+     * whole-number amount of USD to be withdrawn the specified account not to
+     * exceed the account's current balance. After the server processes the
+     * withdrawal request, the server sends one of the following acknowledgments
+     * to the client:
+     * <ul>
+     * <li> {@link #WITHDRAW_SUCCESS}, if account withdrawal succeeds.
+     * <li>{@link #WITHDRAW_FAILURE}, if account withdrawal fails.
+     * </ul>
+     */
+    public static final byte WITHDRAW_REQUEST = 0x40;
+
+    /**
+     * Withdrawal success. Server response to a client's {@link #WITHDRAW_REQUEST}. <br>
+     * Opcode: {@code 0x41} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) balance
+     * </ul>
+     *
+     * The {@code balance} returned reflects the balance in the specified
+     * account after {@code amount} is deposited.
+     */
+    public static final byte WITHDRAW_SUCCESS = 0x41;
+
+    /**
+     * Withdrawal failure. Server response to a client's {@link #WITHDRAW_REQUEST}. <br>
+     * Opcode: {@code 0x42} <br>
+     * Payload:
+     * <ul>
+     * <li>(String) reason
+     * </ul>
+     * This message indicates that the server rejects the
+     * {@link #WITHDRAW_REQUEST} for some reason, for example:
+     * <ul>
+     * <li>invalid account number,
+     * <li>insufficient funds,
+     * <li>invalid deposit amount.
+     * </ul>
+     */
+    public static final byte WITHDRAW_FAILURE = 0x42;
+
+    /**
+     * Balance request from a client to a server. Requests the remaining balance
+     * in a given account. This message should only be sent to a server; if
+     * received by a client it should be ignored. <br>
+     * Opcode: {@code 0x50} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) account_number
+     * </ul>
+     *
+     * <p>
+     * The {@code account_number} is the number of the account whose balance
+     * should be returned. After the server processes the account balance check
+     * request, the server sends one of the following acknowledgments to the
+     * client:
+     * <ul>
+     * <li> {@link #BALANCE_SUCCESS}, if account balance check succeeds.
+     * <li>{@link #BALANCE_FAILURE}, if account balance check fails.
+     * </ul>
+     */
+    public static final byte BALANCE_REQUEST = 0x50;
+
+    /**
+     * Balance check success. Server response to a client's
+     * {@link #BALANCE_REQUEST}. <br>
+     * Opcode: {@code 0x51} <br>
+     * Payload:
+     * <ul>
+     * <li>(int) balance
+     * </ul>
+     *
+     * The {@code balance} returned reflects the current balance in the
+     * specified account.
+     */
+    public static final byte BALANCE_SUCCESS = 0x51;
+
+    /**
+     * Deposit failure. Server response to a client's {@link #BALANCE_REQUEST}. <br>
+     * Opcode: {@code 0x52} <br>
+     * Payload:
+     * <ul>
+     * <li>(String) reason
+     * </ul>
+     * This message indicates that the server rejects the
+     * {@link #BALANCE_REQUEST} for some reason, for example:
+     * <ul>
+     * <li>invalid account number.
+     * </ul>
+     */
+    public static final byte BALANCE_FAILURE = 0x52;
+
+    /**
+     * Request from a client to a server to disconnect and shut down the ATM.
+     * This message should only be sent to a server; if received by a client it
+     * should be ignored. <br>
+     * Opcode: {@code 0x60} <br>
+     * Payload: none
+     *
+     * <p>
+     * This operation cannot fail, as it simply notifies the server of the ATM's
+     * intent to shut down. (There might be cases where the bank wants to keep
+     * the ATM up and running, though we omit them in this protocol). After the
+     * server processes the disconnect request, the server sends
+     * {@link #END_SESSION_SUCCESS} to the client.
+     */
+    public static final byte END_SESSION = 0x60;
+
+    /**
+     * End session success. Server response to a client's {@link #END_SESSION}. <br>
+     * Opcode: {@code 0x61} <br>
+     * Payload: None.
+     */
+    public static final byte END_SESSION_SUCCESS = 0x61;
+
+    /**
+     * Message from the server to the client indicating that a message with an
+     * unknown opcode has been received. On the second receipt of an unknown
+     * opcode, the server will disconnect the client, after sending an
+     * {@link #END_SESSION_SUCCESS} message. Opcode: {@code Ox62}<br>
+     * Payload: None.
+     */
+    public static final byte UNKNOWN_OPCODE = 0x62;
+}
