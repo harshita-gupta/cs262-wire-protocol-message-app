fa7635df768abac212edbc994f174ec2691b38f6
Lisa Vo <lisavo@college.harvard.edu>
2018-02-13 17:36:01 -0500

    updated client.py

--- client.py
+++ client.py
@@ -1,31 +1,42 @@
 # Import socket module
 import config
 import socket
+import clientSend
 
 current_user = None
+
+def getStartupInput():
+    print '''
+WELCOME - type the number of a function:
+    (1) Create Account
+    (2) Log In 
+    '''
+    startupInput = raw_input('>> ')
+    return startupInput
+
+def processInput(requestNumber):
+    #create
+    if requestNumber == str(1):
+        clientSend.create_request(sock)
+        
+    #delete
+    elif requestNumber == str(2):
+        clientSend.login_request(sock)
+        
+    return
 
 if __name__ == '__main__':
         
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        # mySocket.connect ( ( '', int(myPort)) )
-        s.connect(('', 8080))
+		s.connect((raw_input("IP:"), config.port))
     except:
-        print "ERROR: could not connect to " + myHost + ":" + myPort
+        print "ERROR: could not connect to port: " + myPort
         sys.exit()
 
     while True:
-        netBuffer = getInput()
-        #menu selection and function priming
-        processInput(netBuffer)
+        startupInput = getStartupInput()
+        processInput(startupInput)
         getResponse()
 
     mySocket.close()
-
-# connect to the server on local computer
-s.connect((raw_input("IP:"), config.port))
-
-# receive data from the server
-print s.recv(1024)
-# close the connection
-s.close()
