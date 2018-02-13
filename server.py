46e06f0c229e2eb97971a149ef27e2873857615a
harshita-gupta <gupta.harshita.001@gmail.com>
2018-02-13 02:21:14 -0500

    added comments to server code

--- server.py
+++ server.py
@@ -22,17 +22,46 @@ opcodes = {'\x10': myServerReceive.create_request,
            }
 
 
+################################################
+# STATE HANDLING ########################
+################################################
+
+'''
+The server has three primary pieces of state that it needs to handle:
+1. The list of created accounts that have not been deleted
+2. Queues of undelivered messages for a given username, if that user is not
+    currently connected to the server
+3. A mapping from usernames of clients currently logged in to the socket
+    socket objects that represent connections with these clients.
+'''
+
+
 class ActiveClients(object):
+    '''
+    ActiveClients handles the third piece of state described above.
+
+    The primary data structure in Active Clients is a dictionary self.sockets
+    that stores mappings of currently active usernames to their socket objects.
+
+    Along with a lock associated with the top-level dictionary data structure,
+    ActiveClients creates a lock for each spcific socket object.
+
+    This socket-level lock is critical since distinct threads initiated by
+    distinct clients can be interacting with the same socket,
+    this occurs when one client is initiating a message delivery to another
+    client.
+    '''
+
     def __init__(self):
         """
         Constructor.
         @param urls list of urls to check
         @param output file to write urls output
         """
-        self.lock = threading.lock()
+        self.lock = threading.Lock()
 
         # dictionary that stores username as keys and
-        # socket objects as values
+        # (locks, socket objects) as values
         self.sockets = {}
 
     def log_out(self, username):
@@ -40,10 +69,13 @@ class ActiveClients(object):
             return (False, "Username not currently logged in")
         logging.info("Waiting to obtain lock for active socket conns dict.")
         with self.lock:
-            self.sockets[username].close()
-            logging.info(
-                "Successfully closed connection with socket for username %s",
-                username)
+            logging.info("waiting to obtain lock for socket for %s", username)
+            with self.sockets[username][0]:
+                self.sockets[username][1].close()
+                logging.info(
+                    "Successfully closed connection with %s's socket",
+                    username)
+                self.sockets[username] = None
         return True
 
     def log_in(self, username, sock, account_list):
@@ -57,19 +89,28 @@ class ActiveClients(object):
                 if self.sockets.get(username):
                     return (
                         False,
-                        "User %s is already logged in elsewhere." % username)
-                self.sockets[username] = sock
+                        "User %s is already logged in." % username)
+                self.sockets[username] = (threading.Lock(), sock)
         return True
 
 
 class AccountList(object):
+    '''
+    AccountList handles the first and second piece of server state,
+    namely the list of all accounts that are undeleted, and
+    queues of undelivered messages for a given username.
+
+    There are two top-level data structures in AccountList.
+
+    a. The list of accounts self.accounts, which has the lock self.lock
+       associated with it.
+    b. The dictionary that maps usernames to undelivered messages.
+       Each username's undelivered message queue has a lock associated with it.
+    '''
+
     def __init__(self):
-        """
-        Constructor.
-        @param urls list of urls to check
-        @param output file to write urls output
-        """
-        self.lock = threading.lock()
+
+        self.lock = threading.Lock()
 
         # list of all valid usernames
         self.accounts = []
@@ -81,6 +122,7 @@ class AccountList(object):
         self.pending_messages = {}
 
     def add_account(self, username):
+
         # TODO check that username is alphanumeric
         logging.info("Waiting to obtain accountList")
         with self.lock:
@@ -103,6 +145,9 @@ class AccountList(object):
             with self.pending_messages[receiving_user][0]:
                 self.pending_messages[receiving_user].append(message)
         return True
+
+    def delete_account(self, username):
+        return (False, "Not implemented yet")
 
 
 accounts = AccountList()
